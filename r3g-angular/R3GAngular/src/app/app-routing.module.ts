5539f9f7f18b0af6ac0c68e90eb6774aa754c219
Paul Berthelot <>
2021-01-11 16:33:01 +0100

    Paul : routes

--- r3g-angular/R3GAngular/src/app/app-routing.module.ts
+++ r3g-angular/R3GAngular/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AnnotationComponent} from "./module/annotation/annotation.component";
+import {EvaluationComponent} from "./module/evaluation/evaluation.component";
+import {ExplorationComponent} from "./module/exploration/exploration.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'annotation', component: AnnotationComponent},
+  {path: 'evaluation', component: EvaluationComponent},
+  {path: 'exploration', component: ExplorationComponent},
+  {
+    path: '',
+    redirectTo: '/exploration',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
