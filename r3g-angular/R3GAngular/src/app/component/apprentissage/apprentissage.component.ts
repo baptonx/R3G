14425dbb6045085966f52c2ba165280058f04361
hugog <hugo.girault@insa-rennes.fr>
2021-01-15 11:48:13 +0100

    Hugo : clean code

--- r3g-angular/R3GAngular/src/app/component/apprentissage/apprentissage.component.ts
+++ r3g-angular/R3GAngular/src/app/component/apprentissage/apprentissage.component.ts
@@ -20,15 +20,27 @@ export class ApprentissageComponent implements OnInit {
   openDialog():void{
     const dialogRef = this.dialog.open(DialogCSVComponent, {
       data: {
+        hyperparametersNumber:["modeVox","modeLabel","batchSize","dropoutVal","denseSize","denseDropout",
+        "nbFeatureMap","treshCudi","toleranceTresholdMoveCuDi","lossWeigthWindow","lossWeigthClass",
+         "weigthBGclass","weigthBGreg","nb_epoch"],
+         hyperparametersNumberVal:[24,0,3,0.6,5,0.4,2,1.0,0.1,0.01,1,0.01,0.01,1],
+          
         doList: ['true', '1', 't', "doglu"],
         listBN: ['true', '1', 't', "doglu"],
         sBs:['true', '1', 't', "dosbs", "sbs", "sidebyside"],
-        listMS:['true', '1', 't']
+        listMS:['true', '1', 't'],
+        doNormKMA:['true', '1', 't'],
+        evalPredict:['true', '1', 't'],
+        useWo0: ['true', '1', 't'],
+        trainWithGTReg:['true', '1', 't'],
+        doReorientationFor2Sk:['true', '1', 't'],
+        maxPoolSpatial: ['true', '1', 't'],
       }
     });
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
     });
+    
   }
 
   ngOnInit(): void {
@@ -37,8 +49,16 @@ export class ApprentissageComponent implements OnInit {
 
 }
 export interface DialogData {
+  hyperparametersNumber:Array<string>
+  hyperparametersNumberVal:Array<Number>
   doList: Array<string>
   sBs:Array<string>
   listBN:Array<string>
   listMS:Array<string>
+  doNormKMA:Array<string>
+  evalPredict:Array<string>
+  useWo0:Array<string>
+  trainWithGTReg:Array<string>
+  doReorientationFor2Sk:Array<string>
+  maxPoolSpatial: Array<string>
 } 
--- r3g-angular/R3GAngular/src/app/component/dialog-csv/dialog-csv.component.ts
+++ r3g-angular/R3GAngular/src/app/component/dialog-csv/dialog-csv.component.ts
@@ -1,5 +1,6 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSelect } from '@angular/material/select';
 import { DialogData } from '../apprentissage/apprentissage.component';
 
 @Component({
@@ -8,13 +9,43 @@ import { DialogData } from '../apprentissage/apprentissage.component';
   styleUrls: ['./dialog-csv.component.css']
 })
 export class DialogCSVComponent implements OnInit {
+  @ViewChild('doBatchNM') 
+  doBatchNM: ElementRef<MatSelect> | undefined
+  list:Array<Array<number>>
+
+
  
-  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {
-    
+  constructor(
+    public dialogRef: MatDialogRef<DialogCSVComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+    this.list=[]
+    this.list.push([])
    }
 
   ngOnInit(): void {
     console.log(this.data)
+  }
+  saveCSV():void{
+
+    const rows = [
+      ["name1", "city1", "some other info"],
+      ["name2", "city2", "more info"]
+  ];
+  
+  let csvContent = "data:text/csv;charset=utf-8," 
+      + rows.map(e => e.join(",")).join("\n");
+      var encodedUri = encodeURI(csvContent);
+var link = document.createElement("a");
+link.setAttribute("href", encodedUri);
+link.setAttribute("download", "my_data.csv");
+document.body.appendChild(link); // Required for FF
+
+link.click(); // This will download the data file named "my_data.csv".
+    this.dialogRef.close()
+  }
+
+  createNumList():void{
+
   }
 
 }
