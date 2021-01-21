8c5552ec66d0f9bff7682e6b241175d19719a506
Lassalas Thomas <thomas.lassalas@insa-rennes.fr>
2021-01-21 17:49:04 +0100

    add BDD

--- r3g-angular/R3GAngular/src/app/module/exploration/exploration.component.ts
+++ r3g-angular/R3GAngular/src/app/module/exploration/exploration.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {FormControl} from '@angular/forms';
-import {MatInput} from "@angular/material/input";
+import {MatInput} from '@angular/material/input';
 
 @Component({
   selector: 'app-exploration',
@@ -10,15 +9,31 @@ import {MatInput} from "@angular/material/input";
 
 
 export class ExplorationComponent implements OnInit {
-  selectedSeq: Array<string>//Contient les indices des séquences sélectionnées
-  @ViewChild("inputFiltre") inputFiltre! : MatInput;
+  selectedSeq: Array<string>; // Contient les indices des séquences sélectionnées
+  @ViewChild('inputFiltre') inputFiltre!: MatInput;
+  picker = document.getElementById('picker');
+  listing = document.getElementById('listing');
   constructor() {
-    this.selectedSeq = ['sequence 1', 'sequence 2', 'sequence 3','sequence 4', 'sequence 5', 'sequence 6','sequence 7', 'sequence 8', 'sequence 9', 'sequence 10'];
+    this.selectedSeq = ['sequence 1', 'sequence 2', 'sequence 3', 'sequence 4', 'sequence 5', 'sequence 6', 'sequence 7', 'sequence 8', 'sequence 9', 'sequence 10'];
   }
 
   ngOnInit(): void {
   }
   deleteSeq(i: number): void{
     this.selectedSeq.splice(i,1);
+  }
+  importBase(): void{
+
+    // @ts-ignore
+    picker.addEventListener('change', e => {
+      // @ts-ignore
+      for (const file of Array.from(e.target.files)) {
+        const item = document.createElement('li');
+        // @ts-ignore
+        item.textContent = file.webkitRelativePath;
+        // @ts-ignore
+        listing.appendChild(item);
+      };
+    });
   }
 }
