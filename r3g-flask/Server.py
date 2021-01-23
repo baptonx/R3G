d0eecb50f5a953e041cd1ef67603c55750c3f6e7
hugog <hugo.girault@insa-rennes.fr>
2021-01-23 14:08:04 +0100

    test api wandb

---
+++ r3g-flask/Server.py
@@ -0,0 +1,30 @@
+import wandb
+import json
+from flask import Flask
+import numpy as np
+from PIL import Image
+from matplotlib import pyplot as plt
+
+
+app = Flask(__name__)
+api = wandb.Api()
+def startWandb():
+    run = api.run("recoprecoce-intui/36mcvs8u")
+    im = run.file("FilesResult/DetailedConfusionsMatrix/confMatrixAt0.png").download(replace=True)
+    image=Image.open("FilesResult/DetailedConfusionsMatrix/confMatrixAt0.png")
+    print(np.asarray(image))
+
+
+@app.route('/models/getAllNames')
+def index():
+    runs = api.runs("recoprecoce-intui")
+    name_list = []
+    for run in runs:
+        if run.state == 'finished':
+            name_list.append(run.name)
+    return json.dumps(name_list)
+
+
+if __name__ == "__main__":
+    #app.run()
+    startWandb()
