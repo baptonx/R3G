36a65cd6f7c3db79677d7286d9b465ab1267b84b
hugog <hugo.girault@insa-rennes.fr>
2021-02-12 15:18:28 +0100

    download hyperP

--- r3g-flask/Class/Model.py
+++ r3g-flask/Class/Model.py
@@ -1,4 +1,5 @@
 class Model:
-  def __init__(self, _id, name):
+  def __init__(self, _id, name, hyperparam):
+    self.hyperparam = hyperparam
     self.name = name
     self._id = _id
--- r3g-flask/Server.py
+++ r3g-flask/Server.py
@@ -7,18 +7,21 @@ from os import listdir, walk
 from os.path import isfile, join
 import re
 
+from Class.Hyperparameters import Hyperparameters
 from Class.Model import Model
 
 app = Flask(__name__)
 api = wandb.Api()
+runs = api.runs("recoprecoce-intui")
+model_list=[]
 
 
 #On télécharge tous les fichiers liés aux hyperparamètres d'un model. C'est essentiel pour évaluer une séquence
+# Ca peut etre assez long, donc on le fait avant de run le serv
 def downloadHyperparameters():
-    runs = api.runs("recoprecoce-intui")
     for run in runs:
         if not os.path.exists("Hyperparameters/" + run.id):
-            if run.state == 'finished' and run.tags.count('Liu') > 0:
+            if run.state == 'finished' and run.tags.count('maxPoolBetLayers') > 0:
                 for file in run.files():
                     if file.name=="best_val_loss_epochs.txt":
                         file.download("Hyperparameters/"+run.id, replace=True)
@@ -38,18 +41,42 @@ def test():
              #   if p.match(file):
               #      listeFichiers.extend(file)
    # print(listeFichiers)
-    
-#Cette route permet de récupérer la liste des modèles disponible sur Wandb, après filtration (Requete GET)
-@app.route('/models/getModelsNames')
-def index():
-    runs = api.runs("recoprecoce-intui")
-    name_list = []
+
+
+# Au lancement du serveur, on crée des objets de type Model contenant le nom l'id et une liste d'hyperparamètres pour chaque
+# modèles présent sur le board Wandb. On recupère les hyperparamètres grace au fichier best_val_loss_epochs.txt,
+# le nom grace a run.name et l'id grace à run.id
+# param est un dictionnaire permettant de créer des objets Hyperparamètres pour chaque modèle
+def startAPIWandb():
+    param = {}
     for run in runs:
-        if run.state == 'finished' and run.tags.count('Liu')>0:
-            file = api.run("recoprecoce-intui/"+run.id).file("best_val_loss_epochs.txt")
-            m = Model(run.id, run.name)
-            name_list.append(m.__dict__)
-    return json.dumps(name_list)
+        if run.state == 'finished' and run.tags.count('maxPoolBetLayers') > 0:
+            if os.path.exists("Hyperparameters/" + run.id + "/best_val_loss_epochs.txt"):
+                param[run.id] = []
+                with open("Hyperparameters/" + run.id + "/best_val_loss_epochs.txt", "r") as f:
+                    for line in f.readlines():
+                        if "argv:" in line:
+                            for elt in (line.replace("']\n", "").split(":['")[1]).split("', "):
+                                param[run.id].append(Hyperparameters(elt.split("=")[0], elt.split("=")[1]).__dict__)
+                m = Model(run.id, run.name, param[run.id])
+                model_list.append(m.__dict__)
+
+    
+#Cette route permet de récupérer la liste des modèles disponible sur Wandb, après avoir filtré les tags, ainsi que les
+# hyperparamètres stockés dans les fichiers best_val_epochs.txt
+@app.route('/models/getModelsNames')
+def getModelsNames():
+    return json.dumps(model_list)
+
+
+#Cette route permet de récupérer un modèle en donnant son id en paramètre
+@app.route('/models/getModel/<id>')
+def getModel(id):
+    for elt in model_list:
+        if elt["_id"]==id:
+            return json.dumps(elt)
+
+
 
 #cette route permet de recuperer l'ensemble des noms des fichiers
 @app.route('/models/getMetaData')
@@ -61,5 +88,7 @@ def index2():
 if __name__ == "__main__":
     #test()
     downloadHyperparameters()
+    startAPIWandb()
     app.run(host='0.0.0.0')
+
 
