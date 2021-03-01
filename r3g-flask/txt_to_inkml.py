c4b3e7feb02851bc7f5b09084cb1d8a28f6de97a
Gaspard.Levifve <gaspard.levifve@insa-rennes.fr>
2021-03-01 19:43:10 +0100

    correction parser

---
+++ r3g-flask/txt_to_inkml.py
@@ -0,0 +1,81 @@
+"""Convertisseur txt to inkml"""
+
+import sys
+import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import SubElement
+from xml.dom.minidom import parseString
+
+def generate_template():
+    """On genere un template"""
+    root = ET.Element('ink', {"xmlns":"http://www.w3.org/2003/InkML"})
+    trace_format = SubElement(root, 'traceFormat')
+    SubElement(trace_format, 'channel', {"name":"X", "type":"decimal"})
+    SubElement(trace_format, 'channel', {"name":"Y", "type":"decimal"})
+    SubElement(trace_format, 'channel', {"name":"Z", "type":"decimal"})
+    SubElement(trace_format, 'channel', {"name":"timestamp", "type":"decimal"})
+    return ET.ElementTree(root)
+
+def add_labels(inkmltree, labels):
+    """on ajoute les annotations a l'arbre passe en param"""
+    root = inkmltree.getroot()
+    filelabels = open(labels, 'r')
+    for line in filelabels:
+        label = line[:-1].split(',')
+        annotation_xml = SubElement(root, 'annotationXML')
+        annotation_xml.set('type', 'action')
+        annotation = SubElement(annotation_xml, 'annotation')
+        annotation.set('type', 'type')
+        annotation.text = label[0]
+        annotation = SubElement(annotation_xml, 'annotation')
+        annotation.set('type', 'Debut')
+        annotation.text = label[1]
+        annotation = SubElement(annotation_xml, 'annotation')
+        annotation.set('type', 'Fin')
+        annotation.text = label[2]
+
+def add_data(inkmltree, data, fps):
+    """ajout des donnees a l'arbre inkml"""
+    root = inkmltree.getroot()
+    filedata = open(data, 'r')
+    timestamp = 0
+    traces = {}
+    tracegroup = SubElement(root, 'traceGroup')
+    for line in filedata:
+        positions = line[:-1].split(' ')
+        for i in range(int(len(positions) / 3)):
+            traces.setdefault(str(i), [])
+            traces[str(i)] += [[positions[3 * i],
+                                positions[3 * i + 1], positions[3 * i + 2],
+                                str(timestamp)]]
+        timestamp += 1 / fps
+    for articulation in traces:
+        trace = SubElement(tracegroup, 'trace')
+        trace.text = ''
+        for tab in traces[articulation]:
+            for elem in tab:
+                trace.text = str(trace.text) + str(elem) + ' '
+            trace.text = trace.text[:-1] + ', '
+        trace.text = trace.text[:-2]
+
+
+
+if __name__ == "__main__":
+    LABELSPATH = sys.argv[1]
+    DATA = sys.argv[2]
+    INKMLFILEPATH = sys.argv[3]
+    FPS = int(sys.argv[4])
+    INKML_TREE = generate_template()
+    add_labels(INKML_TREE, LABELSPATH)
+    add_data(INKML_TREE, DATA, FPS)
+    open(INKMLFILEPATH, "w")
+    INKML_TREE.write(INKMLFILEPATH, encoding="UTF-8", xml_declaration=True)
+    F1 = open(INKMLFILEPATH, "r")
+    PARSER = parseString(F1.read())
+    F1.close()
+    F = open(INKMLFILEPATH, "w")
+    F.write(PARSER.toprettyxml())
+    F.close()
+
+    #Exemple de commande sur le terminal :
+    #python3 txt_to_InkML.py BDD/datalabel.txt BDD/datanormalise.txt generated.inkml 50
+    #python3 txt_to_InkML.py labelsFile dataFile output.inkml fps
