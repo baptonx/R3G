2a2d12ffc2d6551d64611a8819a45d29d3a743fc
Paul Berthelot <>
2021-04-21 11:27:40 +0200

    Paul class Annotation

--- r3g-angular/R3GAngular/src/app/class/commun/annotation/annotation.ts
+++ r3g-angular/R3GAngular/src/app/class/commun/annotation/annotation.ts
@@ -1,6 +1,11 @@
 export class Annotation {
   public t1!: number;
   public t2!: number;
+  public f1!: number;
+  public f2!: number;
+  public pointAction!: number;
+  public classeGeste!: Array<string>;
+
 
   public verifyT1BeforeT2(): void {
     if (this.t1 > this.t2) {
