d36e3dce00a3d0cfa7ec81ee13b7a86c7c0b7001
Hugo.Girault <hugo.girault@insa-rennes.fr>
2021-04-22 18:51:20 +0200

    gestion des couleurs dans le cache du navigateur

--- r3g-angular/R3GAngular/src/app/component/editeur/editeur.component.ts
+++ r3g-angular/R3GAngular/src/app/component/editeur/editeur.component.ts
@@ -14,22 +14,36 @@ export class EditeurComponent implements OnInit {
   isLinear = false;
   classeGeste:Array<String>=[];
   couleur:Array<String>=[];
+  geste:string="";
   dataSource!: MatTableDataSource<String>;
   displayedColumns=['Geste','Couleur']
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(public http:HttpClient,public bdd:BddService) {
     this.classeGeste=this.bdd.classesGestes;
-    this.classeGeste.forEach(elt=>{
-      this.couleur.push('blue')
-    })
+    for(var i=0;i<this.classeGeste.length;i++){
+    let Col=localStorage.getItem(this.classeGeste[i].toString());
+    if(Col!=null){  //  it checks values here or not to the variable
+       this.couleur.push(Col)
+    }
+    else{
+      this.couleur.push('')
+    }
+  
+    }
     this.dataSource = new MatTableDataSource<String>(this.classeGeste);
     console.log(this.dataSource);
+    console.log(this.couleur)
   }
 
   changeVal(event: any, i:number):void{
     this.couleur[i]=event.target.value
+    this.geste = this.classeGeste[i].toString();
+    localStorage.setItem(this.geste,event.target.value)
+    console.log(localStorage)
 }
+
+
 
   saveColor():void{
     console.log(this.couleur)
